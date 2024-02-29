4b681b528c3e26f3adc7dacaad068e19c25eaf51
Reid Russell <145510043+Reidruss@users.noreply.github.com>
2024-02-28 22:14:20 -0500

    Update solver.cpp

--- solver.cpp
+++ solver.cpp
@@ -5,30 +5,31 @@ using namespace std;
 
 int main(int argc, char **argv){
 
-	int game[N][N] = { { 0, 9, 7, 0, 0, 0, 4, 0, 0 },
-                       { 0, 0, 0, 0, 0, 0, 0, 5, 0 },
-                       { 5, 4, 6, 0, 0, 1, 7, 0, 0 },
+  int game[N][N] = { { 0, 9, 7, 0, 0, 0, 4, 0, 0 },
+                     { 0, 0, 0, 0, 0, 0, 0, 5, 0 },
+                     { 5, 4, 6, 0, 0, 1, 7, 0, 0 },
+	
+                     { 0, 0, 9, 0, 0, 0, 0, 0, 5 },
+                     { 2, 0, 0, 0, 9, 8, 0, 0, 7 },
+                     { 0, 3, 0, 0, 0, 0, 1, 0, 0 },
 
-                       { 0, 0, 9, 0, 0, 0, 0, 0, 5 },
-                       { 2, 0, 0, 0, 9, 8, 0, 0, 7 },
-                       { 0, 3, 0, 0, 0, 0, 1, 0, 0 },
+                     { 6, 0, 0, 0, 4, 0, 0, 1, 0 },
+                     { 0, 0, 8, 5, 3, 0, 0, 2, 0 },
+                     { 0, 0, 0, 6, 0, 0, 0, 0, 0 } };
 
-                       { 6, 0, 0, 0, 4, 0, 0, 1, 0 },
-                       { 0, 0, 8, 5, 3, 0, 0, 2, 0 },
-                       { 0, 0, 0, 6, 0, 0, 0, 0, 0 } };
+  cout << "Initial Puzzle:" << endl << endl;
+	
+  Sudoku sudo;
+  sudo.print(game);
 
-	cout << "Initial Puzzle:" << endl << endl;
-	Sudoku sudo;
-	sudo.print(game);
+  cout << endl;
+  cout << "Computing..." << endl;
+  cout << endl;
 
-	cout << endl;
-	cout << "Computing..." << endl;
-	cout << endl;
-
-	if(sudo.solver(game)){
-	    cout << "Solution Found: " << endl << endl;
-		sudo.print(game);
-	}else
-		cerr << "There is no solution to the given puzzle." << endl;
+  if(sudo.solver(game)){
+    cout << "Solution Found: " << endl << endl;
+    sudo.print(game);
+  }else
+    cerr << "There is no solution to the given puzzle." << endl;
 }
 
